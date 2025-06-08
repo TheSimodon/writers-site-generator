26b0ad05bc94a69a220f732e5a9230d620f435ab
Simodon <david.loeffler91@gmail.com>
2025-06-09 00:31:51 +0200

    Added starting structure

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from textnode import TextNode
+
+
+def main():
+    print("hello, world")
+    textNode = TextNode('This is some anchor text', 'link', 'https://www.boot.dev')
+    print(textNode)
+
+if __name__ == "__main__":
+    main()
